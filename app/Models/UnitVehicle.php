c56de3b62699cc62ec5da41c4c7b7aedae99c7fb
Renan Cavalcante <ocavalcanterenan@gmail.com>
2021-10-30 19:58:01 -0300

    Adicionando funções de criar e deletar Pessoas, Veículos e Pets de uma unidade

--- app/Models/UnitVehicle.php
+++ app/Models/UnitVehicle.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Unitvehicle extends Model
+class UnitVehicle extends Model
 {
     use HasFactory;
 
